719bd0b6ef87bcbf8cc35b392db1e430c8f0b205
mashuang <mashuang@mashuang.local>
2023-06-01 11:45:09 +0800

    feat:add

--- 04-scan.py
+++ 04-scan.py
@@ -1,19 +1,27 @@
-import sys,subprocess,re,os
+import sys,subprocess,re,requests,time,json,configparser,os,traceback,hmac,hashlib,urllib,base64
 from datetime import datetime
 
-root_path               = "/root/code/"
-codeqlJavaFilesPath     = "/root/tools/codeql/java/ql/examples/snippets/"
-codeqlJSFilesPath       = "/root/tools/codeql/javascript/ql/examples/"
-codeqlcppFilesPath      = "/root/tools/codeql/cpp/ql/examples/snippets/"
-trivyFilesPath          = "/root/tools/trivy_0.41.0/"
-mavenDir                = "/root/tools/apache-maven-3.9.2/"
-mavenSettingsFile       = "/root/tools/apache-maven-3.9.2/settings.xml"
-mavenLocalRepositoryDir = "/root/tools/apache-maven-3.9.2/repository"
-delombokJarFilePath     = "/root/tools/lombok.jar"
-codeqlBinPath           = "/root/tools/codeql-cli-bin/codeql"
-javaHome                = "/root/tools/jdk1.8.0_361/bin/java"
-# javaEnvSetting          = "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home;export PATH=$JAVA_HOME/bin:$PATH" #如果默认环境变量已经设置好，直接设置echo 1即可
-javaEnvSetting          = "echo 1"
+############################################################从配置文件中读取信息############################################################
+file    = os.path.join(os.path.dirname(__file__), '/root/tools/config.ini')
+con     = configparser.ConfigParser() # 创建配置文件对象
+con.read(file, encoding='utf-8') # 读取文件
+
+root_path                         =    str(con.get('DependencyToolConfigInfo', 'root_path'))
+codeqlJavaFilesPath               =    str(con.get('DependencyToolConfigInfo', 'codeqlJavaFilesPath'))
+codeqlJSFilesPath                 =    str(con.get('DependencyToolConfigInfo', 'codeqlJSFilesPath'))
+codeqlcppFilesPath                =    str(con.get('DependencyToolConfigInfo', 'codeqlcppFilesPath'))
+trivyFilesPath                    =    str(con.get('DependencyToolConfigInfo', 'trivyFilesPath'))
+mavenDir                          =    str(con.get('DependencyToolConfigInfo', 'mavenDir'))
+mavenSettingsFile                 =    str(con.get('DependencyToolConfigInfo', 'mavenSettingsFile'))
+mavenLocalRepositoryDir           =    str(con.get('DependencyToolConfigInfo', 'mavenLocalRepositoryDir'))
+delombokJarFilePath               =    str(con.get('DependencyToolConfigInfo', 'delombokJarFilePath'))
+codeqlBinPath                     =    str(con.get('DependencyToolConfigInfo', 'codeqlBinPath'))
+javaHome                          =    str(con.get('DependencyToolConfigInfo', 'javaHome'))
+javaEnvSetting                    =    str(con.get('DependencyToolConfigInfo', 'javaEnvSetting'))
+isAtAll                           = str(con.get('DingdingConfigInfo', 'isAtAll'))
+atPersons                         =     con.get('DingdingConfigInfo', 'atPersons').split(",")
+
+
 ###################################工具集###################################
 def getCurrentTime():
     now = datetime.now() # 获取当前时间
@@ -29,6 +37,54 @@ def getCodeRespName(string):
     else: # 如果没有找到匹配
         pass
 
+def Signature_Url():
+    global file,con
+    webhook_url             = str(con.get('DingdingConfigInfo', 'webhook_url')) 
+    dingdingSecret          = str(con.get('DingdingConfigInfo', 'secret'))  
+    try:
+        timestamp = str(round(time.time() * 1000))
+        secret_enc = dingdingSecret.encode('utf-8')
+        string_to_sign = '{}\n{}'.format(timestamp, dingdingSecret)
+        string_to_sign_enc = string_to_sign.encode('utf-8')
+        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+        Post_Url = ("%s&timestamp=%s&sign=%s" % (webhook_url, timestamp, sign))
+        return Post_Url
+    except Exception as e:
+        traceback.print_exc()
+        pass
+# 发送消息
+def sendDingMessage(Content,isAtAll,atPersons):
+    Post_Url=Signature_Url()
+    # 构建请求头部
+    header = {
+        "Content-Type": "application/json",
+        "Charset": "UTF-8"
+    }
+    # 构建请求数据
+    message ={
+        "msgtype": "text",
+        "text": {
+            "content": Content
+        },
+        "at": {
+            "atMobiles":atPersons,
+            "isAtAll": isAtAll
+        }
+    }
+    try:
+        info = requests.post(url=Post_Url,data=json.dumps(message),headers=header)
+        if json.loads(info.text)['errmsg'] == "ok":
+            pass
+        else:
+            print(json.loads(info.text))
+    except Exception as e:
+        traceback.print_exc()
+        time.sleep(3600)
+        msg="Code Scan Monitor机器人挂掉了，先睡眠1小时，如果一小时你依旧看到这条消息，请手工检查一下～"
+        sendDingMessage(msg,isAtAll,atPersons)
+        pass
+
 
 inputParameter1 = str(sys.argv[1])
 inputParameter2 = str(sys.argv[2])
@@ -39,6 +95,7 @@ replaceFileContenCmd            = "find . -maxdepth 1 -type f -name '*.txt' -exe
 DependencyCheckOutputName       = str(getCurrentTime())+"_"+str(getCodeRespName(inputParameter1))+"_DependencyCheck扫描结果.html"
 DependencyCheckCmd              = "sudo dependency-check.sh --project 'myproject' -s  scanPath   -n  --out outPutname".replace("myproject",getCodeRespName(inputParameter1)).replace("outPutname",DependencyCheckOutputName).replace("scanPath",projectRootPath)+";"
 gitCloneCmd                     =  "cd " + str(root_path) + ";"+ inputParameter1 +";"
+codeqlOutName                   =  ""
 ###################################工具集###################################
 if inputParameter2 == "java":
     lombokCommands          = ['javaHome -jar delombokJarFilePath delombok -n --onlyChanged . -d "delombok"'.replace("javaHome",javaHome).replace("delombokJarFilePath",delombokJarFilePath),'find "delombok" -name \'*.java\' -exec sed \'/Generated by delombok/d\' -i \'{}\' \';\'','find "delombok" -name \'*.java\' -exec sed \'/import lombok/d\' -i \'{}\' \';\'','cp -r "delombok/." "./"','rm -rf "delombok"']
@@ -70,14 +127,22 @@ elif inputParameter2 == "c" or inputParameter2 == "cpp" or inputParameter2 == "c
 
 msg="++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++运行下面命令：++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\nfinaCMD \n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++运行上面面命令：++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
-trivyOutPutFilePath         =  str(getCurrentTime())+"_"+str(getCodeRespName(inputParameter1))+"_trivy扫描结果.txt"
-trivyCmd                    =  " "+trivyFilesPath + "trivy fs " + projectRootPath + " --offline-scan   -o " + trivyOutPutFilePath + ";"
+trivyOutPutFilename         =  str(getCurrentTime())+"_"+str(getCodeRespName(inputParameter1))+"_trivy扫描结果.txt"
+trivyCmd                    =  " "+trivyFilesPath + "trivy fs " + projectRootPath + " --offline-scan   -o " + trivyOutPutFilename + ";"
 codeqlDatabasePath          =  root_path + "/" + str(getCurrentTime())+"_"+str(getCodeRespName(inputParameter1))+"_codeqldatabase"
 mvCodeqlDatabaseDirCmd1     =  "mv " + projectRootPath + "/codeqldatabase " + codeqlDatabasePath+";"
 mvCodeqlDatabaseDirCmd2     =  "mv " + codeqlDatabasePath + " " + root_path + "/" +str(getCodeRespName(inputParameter1)) +"/"+ str(getCurrentTime())+"_"+str(getCodeRespName(inputParameter1))+"_codeqldatabase;"
 finalCMD                    =  (gitCloneCmd+finalCMD+mvCodeqlDatabaseDirCmd1 + trivyCmd + replaceFileContenCmd + mvCodeqlDatabaseDirCmd2).replace("javaEnvSetting",javaEnvSetting)
 # finalCMD                    =  gitCloneCmd + "sudo bash -c '''" + finalCMD + "'''"
 msg                         =  msg.replace("finaCMD",finalCMD)
+startTime           = time.time()
 print(msg)
-returned_value = subprocess.call(finalCMD, shell=True) # 返回退出码
+returned_value      = subprocess.call(finalCMD, shell=True) # 返回退出码
 print('returned value:', returned_value)
+endTime             = time.time()
+totalTime           =  str((endTime-startTime)/60) + "min"
+trivyOutPutFilePath = root_path + str(getCodeRespName(inputParameter1)) + "/"+trivyOutPutFilename
+codeqlOutFilePath   = root_path + str(getCodeRespName(inputParameter1)) + "/"+codeqlOutName
+scanResultPath      = trivyOutPutFilePath + "," + codeqlOutFilePath
+msg             = "[✅] getCodeRespName 扫描完成\n[扫描耗时]:".replace("getCodeRespName",str(getCodeRespName(inputParameter1))) +totalTime + "\n[扫描结果]: scanResultPath".replace("scanResultPath",scanResultPath)
+sendDingMessage(msg,isAtAll,atPersons)
